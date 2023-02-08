ba93aef27956dceee28ebcc630995145b641fa90
songhee24 <akaratel61@gmail.com>
2023-02-08 11:16:28 +0600

    created first page

---
+++ lib/src/screens/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+///  Created by mac on 8/2/23.
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

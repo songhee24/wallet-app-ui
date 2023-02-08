5e5b677ead6149e05aa8fc6e9c88e58d28162623
songhee24 <akaratel61@gmail.com>
2023-02-08 11:02:33 +0600

    some init fixes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:wallet_app_ui/main.dart';
+import 'package:wallet_app_ui/src/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

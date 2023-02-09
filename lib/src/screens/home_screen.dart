e9faac6f7aab21d3e9f0c2ebdcfb7032ffe930a3
songhee24 <akaratel61@gmail.com>
2023-02-09 15:28:50 +0600

    check time zone

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -100,6 +100,7 @@ class _HomeScreenState extends State<HomeScreen> {
             const SizedBox(
               height: 12,
             ),
+
             SmoothPageIndicator(
               controller: _controller,
               count: 3,
